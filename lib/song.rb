0d97a640188365dcee45188eea58a29fb33b34bb
Lon Bitner <lonbitner@gmail.com>
2020-05-14 22:27:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  extend Memorable
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :artist
 
